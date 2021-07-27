82d43379089adbb6927d31ef27b39ee0bf5dbdb7
Ahnaf Alfariza <ahnaf.alfariza@shopee.com>
2021-07-27 09:18:55 +0700

    google analytics setup

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,6 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 import { GA_TRACKING_ID } from '../lib/gtag'
-
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
