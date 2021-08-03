ceebb78da55edf571c4ac81ad97f12c51f4c7a26
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-08-03 20:59:59 +0700

    add dummy token

--- constants/dummy.js
+++ constants/dummy.js
@@ -1,5 +1,9 @@
+import tokenData from './json/token.json'
+
 export const PROFILE_IMAGE =
   'https://lh3.googleusercontent.com/qWbbmpNYi9ZfvsitO4gbbO56ljsghbBQxjCTsdlS9pcit5Qp7xhFNdfhD5Sna8rTTInW_OHZ6mBfgVEVlpAEG3ZjVmLQ5Tsf-CB0kAE=w600'
 
 export const COVER_IMAGE =
   'https://img.freepik.com/free-vector/comic-style-wallpaper_79603-1248.jpg?size=626&ext=jpg'
+
+export const TOKEN_DATA = tokenData
