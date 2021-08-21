68ed4a52d2520b1bf7cb1432f2a79d61dc454130
hdriqi <riqi.asdf@gmail.com>
2021-08-21 13:08:30 +0700

    update: parse image utils

--- utils/common.js
+++ utils/common.js
@@ -23,24 +23,35 @@ export const parseImgUrl = (url, defaultValue = '', opts = {}) => {
   if (!url) {
     return defaultValue
   }
-  const [protocol, path] = url.split('://')
-  if (protocol === 'ipfs') {
-    if (opts.useOriginal || process.env.APP_ENV !== 'production') {
-      const cid = new CID(path)
-      if (cid.version === 0) {
-        return `https://ipfs-gateway.paras.id/ipfs/${path}`
-      } else {
-        return `https://ipfs.fleek.co/ipfs/${path}`
+  if (url.includes('://')) {
+    console.log('hmm')
+    const [protocol, path] = url.split('://')
+    if (protocol === 'ipfs') {
+      if (opts.useOriginal || process.env.APP_ENV !== 'production') {
+        const cid = new CID(path)
+        if (cid.version === 0) {
+          return `https://ipfs-gateway.paras.id/ipfs/${path}`
+        } else {
+          return `https://ipfs.fleek.co/ipfs/${path}`
+        }
       }
+
+      let transformationList = []
+      if (opts.width) {
+        transformationList.push(`tr:w-${opts.width}`)
+      } else {
+        transformationList.push('tr:w-0.8')
+      }
+      return `https://cdn.paras.id/${transformationList.join(',')}/${path}`
+    }
+  } else {
+    const cid = new CID(url)
+    if (cid.version === 0) {
+      return `https://ipfs-gateway.paras.id/ipfs/${cid}`
+    } else if (cid.version === 1) {
+      return `https://ipfs.fleek.co/ipfs/${cid}`
     }
 
-    let transformationList = []
-    if (opts.width) {
-      transformationList.push(`tr:w-${opts.width}`)
-    } else {
-      transformationList.push('tr:w-0.8')
-    }
-    return `https://cdn.paras.id/${transformationList.join(',')}/${path}`
+    return url
   }
-  return url
 }
