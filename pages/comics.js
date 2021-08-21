54df9e6a592f1aea755c3f6ac7dc8027b486d29b
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-08-21 12:21:09 +0700

    click comic

--- pages/comics.js
+++ pages/comics.js
@@ -1,18 +1,26 @@
-import Link from 'next/link'
-
 import Head from 'components/Common/Head'
 import Layout from 'components/Layout'
+import { useRouter } from 'next/router'
 
 const Comics = () => {
+  const router = useRouter()
+
+  const onClickComic = () => {
+    router.push('/overview/paradigm')
+  }
+
   return (
     <Layout>
       <Head />
       <div className="max-w-6xl m-auto p-4 py-8">
         <p className="text-white font-bold text-4xl mb-8">Comics</p>
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <div
+          className="grid grid-cols-2 md:grid-cols-4 gap-4"
+          onClick={onClickComic}
+        >
           <img
             src="https://d30womf5coomej.cloudfront.net/sa/62/7b09e240-fd2a-4e9c-ac85-d4b54ea39778_z.jpg"
-            className="rounded-md"
+            className="rounded-md cursor-pointer"
           />
         </div>
       </div>
