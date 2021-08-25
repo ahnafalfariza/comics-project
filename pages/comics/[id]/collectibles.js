9f63cb10313c7d84c264d138f45929bf06b98100
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-08-25 12:19:59 +0700

    ref: collectibles comics

--- pages/comics/[id]/collectibles.js
+++ pages/comics/[id]/collectibles.js
@@ -1,22 +1,15 @@
-import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import Layout from 'components/Layout'
 import Head from 'components/Common/Head'
 import ComicInfo from 'components/Comic/ComicInfo'
-import ChapterCollectibles from 'components/Token/TokenList'
 
-import { COMIC_COLLECTIBLES_DATA } from 'constants/dummy'
 import { parseImgUrl } from 'utils/common'
 import { FETCH_TOKENS_LIMIT } from 'constants/constant'
 import CardList from 'components/Token/CardList'
 
 const Collectibles = ({ comicInfo }) => {
-  const router = useRouter()
-
-  const scrollCollectibles = `${router.query.id}::collectibles`
-
   const [tokens, setTokens] = useState([])
   const [page, setPage] = useState(0)
   const [hasMore, setHasMore] = useState(true)
@@ -69,15 +62,9 @@ const Collectibles = ({ comicInfo }) => {
         image={parseImgUrl(comicInfo.media)}
       />
       <ComicInfo data={comicInfo} defaultIndex={1} />
-      <div className="mt-4">
+      <div className="mt-4 max-w-5xl m-auto">
         <CardList tokens={tokens} hasMore={hasMore} fetchTokens={fetchTokens} />
       </div>
-      {/* <ChapterCollectibles
-        name={scrollCollectibles}
-        tokens={tokens}
-        fetchData={fetchOwnerTokens}
-        hasMore={hasMore}
-      /> */}
     </Layout>
   )
 }
