e38927b63a275946ecdd06368696084dabc5da24
hdriqi <riqi.asdf@gmail.com>
2021-10-25 20:57:23 +0700

    update: header title to Paras Comic

--- components/Common/Head.js
+++ components/Common/Head.js
@@ -1,7 +1,7 @@
 import NextHead from 'next/head'
 
 const Head = ({
-  title = 'Comics by Paras',
+  title = 'Paras Comic',
   description = 'New way to collect digital comics. Read and truly own your digital comics. Engage and support the creators like never before.',
   image = '',
   url = 'https://comics.paras.com',
@@ -9,19 +9,19 @@ const Head = ({
 }) => {
   return (
     <NextHead>
-      <title>{`${title} - Comics by Paras`}</title>
-      <meta name="title" content={`${title} - Comics by Paras`} />
+      <title>{`${title} - Paras Comic`}</title>
+      <meta name="title" content={`${title} - Paras Comic`} />
       <meta name="description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
-      <meta property="og:title" content={`${title} - Comics by Paras`} />
+      <meta property="og:title" content={`${title} - Paras Comic`} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta name="keywords" content={keywords} />
       <meta name="robots" content="index, follow" />
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
-      <meta property="twitter:title" content={`${title} - Comics by Paras`} />
+      <meta property="twitter:title" content={`${title} - Paras Comic`} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={image} />
     </NextHead>
