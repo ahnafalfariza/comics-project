6fe4c14499d47ae5cfe8223f6bfe4ce039ec9db0
Edo Wahdana <49039269+EdoWahdana@users.noreply.github.com>
2022-01-05 20:24:34 +0700

    feat: add react-slick for banner slider (#23)
    * feat: add react-slick for banner slider
    
    * fix: remove duplicate slide

---
+++ components/Common/Carousel.js
@@ -0,0 +1,75 @@
+/* eslint-disable react/display-name */
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+const SampleNextArrow = (props) => {
+  const { onClick } = props
+  return (
+    <div
+      className="before:leading-none group-hover:opacity-100 transition duration-500 ease-in-out opacity-0 block absolute right-3 bg-opacity-0 top-1/2 h-auto w-5 -mt-7 lg:-mt-10 cursor-pointer translate-x-0 -translate-y-1/2 z-10"
+      onClick={onClick}
+    >
+      <div className="rounded-full text-white text-5xl lg:text-6xl">{'>'}</div>
+    </div>
+  )
+}
+
+function SamplePrevArrow(props) {
+  const { onClick } = props
+  return (
+    <div
+      className="before:leading-none group-hover:opacity-100 transition duration-500 ease-in-out opacity-0 block absolute -left-0 bg-opacity-0 top-1/2 h-auto w-5 -mt-7 lg:-mt-10 cursor-pointer translate-x-0 -translate-y-1/2 z-10"
+      onClick={onClick}
+    >
+      <div className="rounded-full text-white text-5xl lg:text-6xl">{'<'}</div>
+    </div>
+  )
+}
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  nextArrow: <SampleNextArrow />,
+  prevArrow: <SamplePrevArrow />,
+  appendDots: (dots) => (
+    <div
+      style={{
+        padding: '10px',
+        marginBottom: '30px',
+        zIndex: '10',
+      }}
+    >
+      <ul> {dots} </ul>
+    </div>
+  ),
+  autoplay: true,
+  autoplaySpeed: 3000,
+}
+
+const Carousel = () => {
+  return (
+    <div className="group max-w-6xl w-full mx-auto -mt-10 mb-20">
+      <Slider {...settings}>
+        <div className="w-60 h-30">
+          <a
+            href="https://ipfs.fleek.co/ipfs/bafybeiaam3zvrf6ar57peyn7n2z2yevfiddsaor5yiaphbfwbmvahw53nq"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="m-auto w-full h-full object-cover focus:outline-none active:outline-none"
+              src="https://ipfs.fleek.co/ipfs/bafybeieplac4arkilsdbxymdc3wlwgujbpqdics4jkcfg4g2o364n27cpu"
+            />
+          </a>
+        </div>
+      </Slider>
+    </div>
+  )
+}
+
+export default Carousel
