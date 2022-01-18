4bc3698e6b2dc1af80d7924d1551942076362be2
EdoWahdana <wahdanaedo@gmail.com>
2022-01-18 17:46:09 +0700

    update: common components

--- components/Common/Footer.js
+++ components/Common/Footer.js
@@ -1,110 +1,109 @@
-import useStore from 'lib/store'
 import Link from 'next/link'
 
 const Footer = () => {
-  const setToastConfig = useStore((state) => state.setToastConfig)
-
-  const _showBetaInfo = () => {
-    setToastConfig({
-      text: (
-        <div className="text-sm text-gray-900">
-          <p>
-            Paras' smart contract is not yet audited by well-known security
-            organization or firm. Use at your own risk!
-          </p>
-        </div>
-      ),
-      type: 'info',
-      duration: null,
-    })
-  }
-
   return (
-    <div className="max-w-6xl w-full m-auto px-4 flex flex-wrap items-end justify-center md:justify-between mt-20 text-sm text-gray-200 z-10 relative pb-4">
+    <div className="max-w-6xl w-full m-auto px-4 flex flex-row justify-center mt-20 text-sm text-black z-10 relative pb-4">
       <div className="py-2">
-        <div className="flex flex-wrap justify-center md:justify-start -mx-2">
-          <div className="flex items-center pt-2 px-2">
-            <Link href="/comics">
-              <a className="flex cursor-pointer ">Comics</a>
-            </Link>
-          </div>
-          <div className="flex items-center pt-2 px-2">
-            <Link href="/market">
-              <a className="flex cursor-pointer ">Market</a>
-            </Link>
-          </div>
-          <div className="flex items-center pt-2 px-2">
-            <Link href="/faq">
-              <a className="flex cursor-pointer ">FAQ</a>
-            </Link>
-          </div>
-        </div>
         <div className="flex flex-wrap justify-center -mx-2">
-          <div className="flex items-center pt-2 px-2">
+          <div className="flex items-center pt-2 px-6">
             <a
               href="https://twitter.com/ParasHQ"
               target="_blank"
               className="flex cursor-pointer "
               rel="noreferrer"
             >
-              Twitter
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                style={{ fill: '#00BBDB' }}
+              >
+                <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z"></path>
+              </svg>
             </a>
           </div>
-          <div className="flex items-center pt-2 px-2">
+          <div className="flex items-center pt-2 px-6">
             <a
               href="https://instagram.com/paras.hq"
               target="_blank"
               className="flex cursor-pointer "
               rel="noreferrer"
             >
-              Instagram
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 50 50"
+                style={{ fill: '#00BBDB' }}
+              >
+                <path d="M 16 3 C 8.83 3 3 8.83 3 16 L 3 34 C 3 41.17 8.83 47 16 47 L 34 47 C 41.17 47 47 41.17 47 34 L 47 16 C 47 8.83 41.17 3 34 3 L 16 3 z M 37 11 C 38.1 11 39 11.9 39 13 C 39 14.1 38.1 15 37 15 C 35.9 15 35 14.1 35 13 C 35 11.9 35.9 11 37 11 z M 25 14 C 31.07 14 36 18.93 36 25 C 36 31.07 31.07 36 25 36 C 18.93 36 14 31.07 14 25 C 14 18.93 18.93 14 25 14 z M 25 16 C 20.04 16 16 20.04 16 25 C 16 29.96 20.04 34 25 34 C 29.96 34 34 29.96 34 25 C 34 20.04 29.96 16 25 16 z"></path>
+              </svg>
             </a>
           </div>
-          <div className="flex items-center pt-2 px-2">
+          <div className="flex items-center pt-2 px-6">
             <a
               href="https://t.me/parashq"
               target="_blank"
               className="flex cursor-pointer "
               rel="noreferrer"
             >
-              Telegram
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 50 50"
+                style={{ fill: '#00BBDB' }}
+              >
+                <path d="M46.137,6.552c-0.75-0.636-1.928-0.727-3.146-0.238l-0.002,0C41.708,6.828,6.728,21.832,5.304,22.445	c-0.259,0.09-2.521,0.934-2.288,2.814c0.208,1.695,2.026,2.397,2.248,2.478l8.893,3.045c0.59,1.964,2.765,9.21,3.246,10.758	c0.3,0.965,0.789,2.233,1.646,2.494c0.752,0.29,1.5,0.025,1.984-0.355l5.437-5.043l8.777,6.845l0.209,0.125	c0.596,0.264,1.167,0.396,1.712,0.396c0.421,0,0.825-0.079,1.211-0.237c1.315-0.54,1.841-1.793,1.896-1.935l6.556-34.077	C47.231,7.933,46.675,7.007,46.137,6.552z M22,32l-3,8l-3-10l23-17L22,32z"></path>
+              </svg>
             </a>
           </div>
-          <div className="flex items-center pt-2 px-2">
+          <div className="flex items-center pt-2 px-6">
             <a
               href="https://discord.gg/vWR2XBNupg"
               target="_blank"
               className="flex cursor-pointer "
               rel="noreferrer"
             >
-              Discord
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                style={{ fill: '#00BBDB' }}
+              >
+                <path d="M25.12,6.946c-2.424-1.948-6.257-2.278-6.419-2.292c-0.256-0.022-0.499,0.123-0.604,0.357 c-0.004,0.008-0.218,0.629-0.425,1.228c2.817,0.493,4.731,1.587,4.833,1.647c0.478,0.278,0.638,0.891,0.359,1.368 C22.679,9.572,22.344,9.75,22,9.75c-0.171,0-0.343-0.043-0.501-0.135C21.471,9.598,18.663,8,15.002,8 C11.34,8,8.531,9.599,8.503,9.615C8.026,9.892,7.414,9.729,7.137,9.251C6.86,8.775,7.021,8.164,7.497,7.886 c0.102-0.06,2.023-1.158,4.848-1.65c-0.218-0.606-0.438-1.217-0.442-1.225c-0.105-0.235-0.348-0.383-0.604-0.357 c-0.162,0.013-3.995,0.343-6.451,2.318C3.564,8.158,1,15.092,1,21.087c0,0.106,0.027,0.209,0.08,0.301 c1.771,3.11,6.599,3.924,7.699,3.959c0.007,0.001,0.013,0.001,0.019,0.001c0.194,0,0.377-0.093,0.492-0.25l1.19-1.612 c-2.61-0.629-3.99-1.618-4.073-1.679c-0.444-0.327-0.54-0.953-0.213-1.398c0.326-0.443,0.95-0.541,1.394-0.216 C7.625,20.217,10.172,22,15,22c4.847,0,7.387-1.79,7.412-1.808c0.444-0.322,1.07-0.225,1.395,0.221 c0.324,0.444,0.23,1.066-0.212,1.392c-0.083,0.061-1.456,1.048-4.06,1.677l1.175,1.615c0.115,0.158,0.298,0.25,0.492,0.25 c0.007,0,0.013,0,0.019-0.001c1.101-0.035,5.929-0.849,7.699-3.959c0.053-0.092,0.08-0.195,0.08-0.301 C29,15.092,26.436,8.158,25.12,6.946z M11,19c-1.105,0-2-1.119-2-2.5S9.895,14,11,14s2,1.119,2,2.5S12.105,19,11,19z M19,19 c-1.105,0-2-1.119-2-2.5s0.895-2.5,2-2.5s2,1.119,2,2.5S20.105,19,19,19z"></path>
+              </svg>
             </a>
           </div>
         </div>
-      </div>
-      <div className="py-2">
-        <div className="flex items-center justify-center md:justify-start">
-          <div className="flex">
-            <p>This project is in public beta.</p>
-            <svg
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              className="inline	ml-2 cursor-pointer opacity-75"
-              onClick={_showBetaInfo}
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M0 8C0 12.4183 3.58172 16 8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8ZM14 8C14 11.3137 11.3137 14 8 14C4.68629 14 2 11.3137 2 8C2 4.68629 4.68629 2 8 2C11.3137 2 14 4.68629 14 8ZM7 10V9.5C7 8.28237 7.42356 7.68233 8.4 6.95C8.92356 6.55733 9 6.44904 9 6C9 5.44772 8.55229 5 8 5C7.44772 5 7 5.44772 7 6H5C5 4.34315 6.34315 3 8 3C9.65685 3 11 4.34315 11 6C11 7.21763 10.5764 7.81767 9.6 8.55C9.07644 8.94267 9 9.05096 9 9.5V10H7ZM9.00066 11.9983C9.00066 12.5506 8.55279 12.9983 8.00033 12.9983C7.44786 12.9983 7 12.5506 7 11.9983C7 11.4461 7.44786 10.9983 8.00033 10.9983C8.55279 10.9983 9.00066 11.4461 9.00066 11.9983Z"
-                fill="white"
-              />
-            </svg>
+        <div className="flex flex-row flex-wrap justify-center mt-2 -mx-2">
+          <div className="flex items-center pt-2 px-4">
+            <Link href="/comics">
+              <a className="flex cursor-pointer hover:text-primary">Comics</a>
+            </Link>
+          </div>
+          <div className="flex items-center pt-2 px-4">
+            <Link href="/market">
+              <a className="flex cursor-pointer hover:text-primary">Market</a>
+            </Link>
+          </div>
+          <div className="flex items-center pt-2 px-4">
+            <Link href="/faq">
+              <a className="flex cursor-pointer hover:text-primary">FAQ</a>
+            </Link>
           </div>
         </div>
-        <div className="mt-2 flex items-center">
+        <div className="mt-2 flex flex-row justify-center text-black text-opacity-40">
           <p>2021 Paras</p>
           <p className="mx-2">|</p>
           <div className="flex items-center text-sm">
