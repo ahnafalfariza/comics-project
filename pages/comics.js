21e7294bdc8da0e9dae74cd9946717b81c5c8830
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-01-19 12:57:39 +0700

    refactor: comic page style

--- pages/comics.js
+++ pages/comics.js
@@ -64,7 +64,12 @@ const Comics = () => {
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8 overflow-hidden">
             {comics.map((data, i) => (
               <div key={i}>
-                <div className="mb-2">
+                <div
+                  className="mb-2 cursor-pointer hover:opacity-90 transition ease-in-out duration-300"
+                  onClick={() =>
+                    router.push(`/comics/${data.comic_id}/chapter`)
+                  }
+                >
                   <Token
                     imgUrl={parseImgUrl(data.media, null, {
                       width: `300`,
@@ -118,7 +123,8 @@ const ComicLoader = () => (
             width="100%"
             height="100%"
             viewBox="0 0 275 380"
-            backgroundColor="#1D1D1D"
+            backgroundColor="#F4F4F5"
+            foregroundColor="#E4E4E7"
           >
             <rect x="0" y="0" rx="0" ry="0" width="275" height="380" />
           </ContentLoader>
