715bcb00ed572ef2d44944578b8438ab3599a01e
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2022-01-19 11:37:47 +0700

    refactor: submission

--- pages/submission.js
+++ pages/submission.js
@@ -1,6 +1,7 @@
 import { InputText, InputTextarea } from 'components/Common/form'
 import Layout from 'components/Common/Layout'
 import useStore from 'lib/store'
+import Head from 'components/Common/Head'
 import React, { useEffect, useRef, useState } from 'react'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 import { FormProvider, useForm } from 'react-hook-form'
@@ -8,10 +9,10 @@ import { readFileAsUrl } from 'utils/common'
 
 const Submission = () => {
   const [coverComic, setCoverComic] = useState('')
-  const [coverComicPreview, setCoverComicPreview] = useState('')
   const [items, setItems] = useState([])
   const [modalGenre, setModalGenre] = useState(false)
   const methods = useForm()
+  const { register, handleSubmit, formState } = methods
   const genreList = ['Romance', 'Superhero', 'Horror', 'Non-Fiction']
   const [errorMessage, setErrorMessage] = useState(false)
   const genreModalRef = useRef()
@@ -118,13 +119,7 @@ const Submission = () => {
   }
 
   const removeFile = (_item) => {
-    items.forEach((item, index) => {
-      if (item.id == _item.id) {
-        delete items[index]
-      }
-    })
-    let filteredList = items.filter((item) => item)
-    setItems(filteredList)
+    setItems(items.filter((item) => item.id !== _item.id))
   }
 
   const inputFileCoverComic = (event) => {
@@ -135,11 +130,10 @@ const Submission = () => {
       if (size < 10) {
         let binaryData = []
         binaryData.push(img)
-        setCoverComicPreview(
+        setCoverComic(
           window.URL.createObjectURL(new Blob(binaryData, { type: 'image/*' }))
         )
         setErrorMessage(false)
-        return setCoverComic(img)
       } else {
         setErrorMessage(true)
       }
@@ -148,14 +142,14 @@ const Submission = () => {
 
   return (
     <Layout>
+      <Head title="Submission" />
       <div className="max-w-6xl m-auto p-4 py-8">
         <h1 className="text-center font-bold text-3xl mb-9">Submission</h1>
         <div className="max-w-3xl m-auto p-4 py-8">
           <FormProvider {...methods}>
-            {/* <form action="https://formspree.io/f/myyokkgo" method="post"> */}
             <form
               id="form-submission"
-              onSubmit={methods.handleSubmit(onSubmit)}
+              onSubmit={handleSubmit(onSubmit)}
               method="post"
             >
               <div className="md:flex justify-between">
@@ -163,11 +157,11 @@ const Submission = () => {
                   <label className="font-bold text-md">Cover Comic</label>
                   <div
                     className={`relative cursor-pointer ${
-                      coverComicPreview === '' ? 'w-40 h-40 ' : 'w-60 h-80'
+                      coverComic === '' ? 'w-40 h-40 ' : 'w-60 h-80'
                     } overflow-hidden rounded-md mt-2 hover:opacity-80`}
                   >
                     <input
-                      {...methods.register('coverComic', { required: true })}
+                      {...register('coverComic', { required: true })}
                       className="cursor-pointer w-full opacity-0 absolute inset-0 z-20"
                       onChange={(event) => inputFileCoverComic(event)}
                       type="file"
@@ -175,21 +169,14 @@ const Submission = () => {
                     />
                     <div
                       className={`${
-                        coverComicPreview === ''
-                          ? 'w-40 h-40 '
-                          : 'w-full h-full'
+                        coverComic === '' ? 'w-40 h-40 ' : 'w-full h-full'
                       } overflow-hidden bg-comic-gray-secondary shadow-inner relative input-text`}
                     >
                       <img
-                        src={
-                          coverComicPreview === '' || coverComic === undefined
-                            ? null
-                            : coverComicPreview
-                        }
+                        src={coverComic === '' ? null : coverComic}
                         className="w-full h-full object-cover cover-comic"
-                        style={{ textIndent: '-10000px' }}
                       />
-                      {coverComicPreview === '' || coverComic === undefined ? (
+                      {coverComic === '' ? (
                         <svg
                           className="w-14 h-14 text-comic-gray-tertiary absolute inset-0 mx-auto my-auto"
                           fill="none"
@@ -209,20 +196,15 @@ const Submission = () => {
                       )}
                     </div>
                   </div>
-                  {methods.formState.errors.coverComic && (
-                    <span
-                      className={`${coverComic ? 'hidden ' : 'text-red-500'}`}
-                    >
-                      This field is required
-                    </span>
-                  )}
-                  {errorMessage && (
+                  {(formState.errors.coverComic || errorMessage) && (
                     <span
                       className={`${
-                        !errorMessage ? 'hidden ' : 'text-red-500'
+                        !coverComic || errorMessage ? 'text-red-500' : 'hidden'
                       }`}
                     >
-                      Image must be less than 10mb
+                      {errorMessage
+                        ? 'Image must be less than 10mb'
+                        : 'This field is required'}
                     </span>
                   )}
                   <div>
@@ -240,9 +222,9 @@ const Submission = () => {
                     type="text"
                     placeholder="Paradigm"
                     className="mt-3 mr-64"
-                    {...methods.register('comicTitle', { required: true })}
+                    {...register('comicTitle', { required: true })}
                   />
-                  {methods.formState.errors.comicTitle && (
+                  {formState.errors.comicTitle && (
                     <span className="text-red-500">This field is required</span>
                   )}
                 </div>
@@ -251,7 +233,7 @@ const Submission = () => {
                   <div>
                     <select
                       className="flex justify-between items-center relative w-full md:w-64 mt-3 px-2 py-2 rounded-lg bg-comic-gray-secondary cursor-pointer input-text"
-                      {...methods.register('genreComic', {
+                      {...register('genreComic', {
                         required: true,
                       })}
                     >
@@ -276,9 +258,9 @@ const Submission = () => {
                   className="resize-none h-40 mt-3"
                   type="text"
                   placeholder="Synoosis of your comic"
-                  {...methods.register('premise', { required: true })}
+                  {...register('premise', { required: true })}
                 />
-                {methods.formState.errors.premise && (
+                {formState.errors.premise && (
                   <span className="text-red-500">This field is required</span>
                 )}
               </div>
@@ -289,9 +271,9 @@ const Submission = () => {
                   type="email"
                   placeholder="paradigm@paras.id"
                   width="80"
-                  {...methods.register('email', { required: true })}
+                  {...register('email', { required: true })}
                 />
-                {methods.formState.errors.email && (
+                {formState.errors.email && (
                   <span className="text-red-500">This field is required</span>
                 )}
               </div>
@@ -305,7 +287,7 @@ const Submission = () => {
                       <input
                         multiple
                         type="file"
-                        {...methods.register('comicContent', {
+                        {...register('comicContent', {
                           required: true,
                           onChange: addImages,
                         })}
@@ -344,23 +326,7 @@ const Submission = () => {
                                 )}
                               >
                                 <img src={item.content} />
-
                                 <svg
-                                  onClick={() => removeFile(item)}
-                                  className="m-2 w-7 h-7 p-1 cursor-pointer text-red-500 bg-white bg-opacity-40 hover:bg-opacity-20 rounded-full absolute top-0 right-0 z-10"
-                                  fill="none"
-                                  stroke="currentColor"
-                                  viewBox="0 0 24 24"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                  ></path>
-                                </svg>
-                                {/* <svg
                                   onClick={() => removeFile(item)}
                                   className="m-2 w-7 h-7 p-1 cursor-pointer text-red-500 bg-white bg-opacity-40 hover:bg-opacity-20 rounded-full absolute top-0 right-0 z-10"
                                   fill="none"
@@ -374,7 +340,7 @@ const Submission = () => {
                                     strokeWidth="2"
                                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                                   ></path>
-                                </svg> */}
+                                </svg>
                               </div>
                             )}
                           </Draggable>
@@ -384,7 +350,7 @@ const Submission = () => {
                     )}
                   </Droppable>
                 </DragDropContext>
-                {methods.formState.errors.comicContent && (
+                {formState.errors.comicContent && (
                   <span
                     className={`${
                       items.length !== 0 ? 'hidden ' : 'text-red-500'
