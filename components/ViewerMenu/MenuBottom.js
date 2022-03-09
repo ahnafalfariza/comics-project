484470b5b459048c52de25222dc6c2a1217ab1cf
Iqbal Utomo <73095492+iqbalutomo@users.noreply.github.com>
2022-03-09 18:36:50 +0700

    feat: preview mode (#79)

--- components/ViewerMenu/MenuBottom.js
+++ components/ViewerMenu/MenuBottom.js
@@ -1,81 +1,127 @@
 import { forwardRef } from 'react'
 
 import { IconChevron, IconGift, IconMessageCircle } from 'components/Icons'
+
+import near from 'lib/near'
 import useStore from 'lib/store'
+
 import { useRouter } from 'next/router'
+import { formatNearAmount } from 'near-api-js/lib/utils/format'
 
 const SHOW_SUPPORT = false
 
-const MenuBottom = forwardRef(({ showMenu, data, hasNext }, ref) => {
-  const setShowComment = useStore((state) => state.setShowComment)
-  const router = useRouter()
+const MenuBottom = forwardRef(
+  (
+    { showMenu, data, hasNext, isPreview = false, isLoginModal = false },
+    ref
+  ) => {
+    const buyChapter = useStore((state) => state.buyChapter)
+    const setShowComment = useStore((state) => state.setShowComment)
+    const router = useRouter()
 
-  const onClickNextChapter = () => {
-    if (hasNext) {
-      router.push({
-        pathname: `/viewer/${data.metadata.comic_id}/${
-          parseInt(data.metadata.chapter_id) + 1
-        }`,
-      })
+    const textRight = () => {
+      return data?.status === 'read'
+        ? 'Read'
+        : data?.price
+        ? `${
+            data?.price === '0' ? 'Free' : `${formatNearAmount(data?.price)} Ⓝ`
+          }`
+        : data?.is_non_mintable
+        ? 'Sold Out'
+        : 'Coming Soon'
     }
-  }
 
-  const onClickPrevChapter = () => {
-    if (parseInt(data.metadata.chapter_id) !== 1) {
-      router.push({
-        pathname: `/viewer/${data.metadata.comic_id}/${
-          parseInt(data.metadata.chapter_id) - 1
-        }`,
-      })
+    const onBuyChapter = () => {
+      if (!near.currentUser) {
+        isLoginModal(true)
+        return
+      }
+      buyChapter(data.token_series_id, data.price)
     }
-  }
 
-  return (
-    <div
-      ref={ref}
-      className={`fixed inset-x-0 bottom-0 overflow-hidden transform z-10 transition-transform ease-in-out duration-300 border-t-2 ${
-        showMenu ? 'translate-y-0' : 'translate-y-20'
-      }`}
-    >
-      <div className="bg-white relative">
-        <div className="flex max-w-xl m-auto p-4 items-center justify-between">
-          <div className="flex items-center">
-            {SHOW_SUPPORT && (
-              <div className="flex items-center cursor-pointer mr-4">
-                <IconGift size={20} color="#ffffff" />
-                <p className="text-black ml-2 text-sm">Support</p>
-              </div>
+    const onClickNextChapter = () => {
+      if (hasNext) {
+        router.push({
+          pathname: `/viewer/${data.metadata.comic_id}/${
+            parseInt(data.metadata.chapter_id) + 1
+          }`,
+        })
+      }
+    }
+
+    const onClickPrevChapter = () => {
+      if (parseInt(data.metadata.chapter_id) !== 1) {
+        router.push({
+          pathname: `/viewer/${data.metadata.comic_id}/${
+            parseInt(data.metadata.chapter_id) - 1
+          }`,
+        })
+      }
+    }
+
+    return (
+      <div
+        ref={ref}
+        className={`fixed inset-x-0 bottom-0 overflow-hidden transform z-10 transition-transform ease-in-out duration-300 border-t-2 ${
+          showMenu ? 'translate-y-0' : 'translate-y-20'
+        }`}
+      >
+        <div className="bg-white relative">
+          <div className="flex max-w-xl m-auto p-2 items-center justify-between">
+            {!isPreview ? (
+              <>
+                <div className="flex items-center">
+                  {SHOW_SUPPORT && (
+                    <div className="flex items-center cursor-pointer mr-4">
+                      <IconGift size={20} color="#ffffff" />
+                      <p className="text-black ml-2 text-sm">Support</p>
+                    </div>
+                  )}
+                  <div
+                    className="flex items-center cursor-pointer"
+                    onClick={setShowComment}
+                  >
+                    <IconMessageCircle size={20} color="#000000" />
+                    <p className="text-black ml-2 text-sm">Comment</p>
+                  </div>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <IconChevron
+                    color="#ffffff"
+                    className={`cursor-pointer transform rotate-180 ${
+                      parseInt(data?.metadata.chapter_id) === 1
+                        ? 'opacity-40'
+                        : ''
+                    }`}
+                    onClick={onClickPrevChapter}
+                  />
+                  <p className="text-black">
+                    Ch {parseInt(data?.metadata.chapter_id)}
+                  </p>
+                  <IconChevron
+                    color="#ffffff"
+                    className={`cursor-pointer ${!hasNext ? 'opacity-40' : ''}`}
+                    onClick={onClickNextChapter}
+                  />
+                </div>
+              </>
+            ) : (
+              <>
+                <p className="pr-4 text-gray-400">Preview Mode</p>
+                <h4
+                  className="text-sm font-semibold bg-primary rounded-full uppercase tracking-wider leading-none px-5 py-3 text-white cursor-pointer"
+                  onClick={onBuyChapter}
+                >
+                  {textRight()}
+                </h4>
+              </>
             )}
-            <div
-              className="flex items-center cursor-pointer"
-              onClick={setShowComment}
-            >
-              <IconMessageCircle size={20} color="#000000" />
-              <p className="text-black ml-2 text-sm">Comment</p>
-            </div>
-          </div>
-          <div className="flex items-center space-x-2">
-            <IconChevron
-              color="#ffffff"
-              className={`cursor-pointer transform rotate-180 ${
-                parseInt(data?.metadata.chapter_id) === 1 ? 'opacity-40' : ''
-              }`}
-              onClick={onClickPrevChapter}
-            />
-            <p className="text-black">
-              Ch {parseInt(data?.metadata.chapter_id)}
-            </p>
-            <IconChevron
-              color="#ffffff"
-              className={`cursor-pointer ${!hasNext ? 'opacity-40' : ''}`}
-              onClick={onClickNextChapter}
-            />
           </div>
         </div>
       </div>
-    </div>
-  )
-})
+    )
+  }
+)
 MenuBottom.displayName = 'MenuBottom'
 
 export default MenuBottom
---
+++ pages/preview/[comicId]/[previewId].js
@@ -0,0 +1,157 @@
+import { useEffect, useRef, useState } from 'react'
+
+import Layout from 'components/Common/Layout'
+import MenuTop from 'components/ViewerMenu/MenuTop'
+import MenuBottom from 'components/ViewerMenu/MenuBottom'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import Head from 'components/Common/Head'
+import BuyChapterModal from 'components/Modal/BuyChapterModal'
+import ChapterNotAvailableModal from 'components/Modal/ChapterNotAvailableModal'
+import LoginModal from 'components/Modal/LoginModal'
+import { BounceLoader } from 'react-spinners'
+import { parseImgUrl } from 'utils/common'
+
+const PreviewImagePage = ({ file, isLoading }) => {
+  return !isLoading ? (
+    <div className="max-w-3xl m-auto relative">
+      <img src={`${parseImgUrl(file)}`} />
+      <div className="absolute inset-0 bg-transparent z-0" />
+    </div>
+  ) : (
+    <div className="h-96 flex justify-center items-center gray">
+      <BounceLoader loading={true} color={'rgb(107, 114, 128)'} size={24} />
+    </div>
+  )
+}
+
+const ChapterPreview = () => {
+  const menuTopRef = useRef()
+  const menuBottomRef = useRef()
+  const previewRef = useRef()
+  const router = useRouter()
+
+  const [showMenu, setShowMenu] = useState(true)
+  const [chapterData, setChapterData] = useState(null)
+  const [previewData, setPreviewData] = useState([])
+  const [showLogin, setShowLogin] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [isBuyChapterModal, setIsBuyChapterModal] = useState(false)
+
+  const { comicId, previewId } = router.query
+
+  useEffect(() => {
+    const handleClickOutsideMenu = (event) => {
+      if (
+        !menuTopRef?.current?.contains(event.target) &&
+        !menuBottomRef?.current?.contains(event.target)
+      ) {
+        setShowMenu(!showMenu)
+      }
+    }
+
+    const handleScroll = () => {
+      setShowMenu(false)
+      let documentHeight = document.body.scrollHeight
+      let currentScroll = window.scrollY + window.innerHeight
+      let modifier = 1
+
+      if (previewData?.length === 0) {
+        modifier = -1
+      }
+
+      if (currentScroll + modifier > documentHeight) {
+        setIsBuyChapterModal(true)
+        setShowMenu(true)
+      }
+    }
+
+    document.addEventListener('scroll', handleScroll)
+    document.addEventListener('mousedown', handleClickOutsideMenu)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutsideMenu)
+      document.removeEventListener('scroll', handleScroll)
+    }
+  }, [
+    menuTopRef,
+    menuBottomRef,
+    previewRef,
+    showMenu,
+    chapterData?.status,
+    previewData?.length,
+  ])
+
+  useEffect(() => {
+    if (comicId && previewId) {
+      fetchChapterData(comicId, previewId)
+      fetchPreviewData(comicId, previewId)
+    }
+  }, [previewId, comicId])
+
+  const onCloseBuyChapterModal = () => {
+    setIsBuyChapterModal(!isBuyChapterModal)
+  }
+
+  const fetchChapterData = async (comicId, previewId) => {
+    const response = await axios.get(`${process.env.COMIC_API_URL}/chapters`, {
+      params: {
+        comic_id: comicId,
+        chapter_ids: [previewId, parseInt(previewId) + 1],
+      },
+    })
+
+    const _chapterData = response.data.data.results[0]
+    setChapterData(_chapterData || null)
+  }
+
+  const fetchPreviewData = async (comicId, previewId) => {
+    setIsLoading(true)
+
+    const response = await axios.get(
+      `${process.env.COMIC_API_URL}/pages-preview/${comicId}/${previewId}`
+    )
+
+    const _previewData = response.data.data
+    setPreviewData(_previewData)
+
+    setIsLoading(false)
+  }
+
+  return (
+    <Layout showNav={false} showFooter={false} className="bg-white">
+      <Head />
+      <ChapterNotAvailableModal
+        show={previewData?.length === (0 || undefined)}
+      />
+      <BuyChapterModal
+        active={isBuyChapterModal || false}
+        data={chapterData}
+        onClose={onCloseBuyChapterModal}
+        hideCloseButton={false}
+        isPreview
+      />
+      <MenuTop
+        ref={menuTopRef}
+        showMenu={showMenu}
+        data={chapterData}
+        isPreview
+      />
+      <MenuBottom
+        ref={menuBottomRef}
+        showMenu={showMenu}
+        data={chapterData}
+        isLoginModal={(event) => setShowLogin(event)}
+        isPreview
+      />
+      <div ref={previewRef} className="min-h-screen h-screen">
+        {previewData?.map((item, idx) => (
+          <PreviewImagePage key={idx} file={item} isLoading={isLoading} />
+        ))}
+      </div>
+      <LoginModal onClose={() => setShowLogin(false)} show={showLogin} />
+    </Layout>
+  )
+}
+
+export default ChapterPreview
