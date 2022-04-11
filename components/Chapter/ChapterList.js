a9154b3e84ae796eb90692c7ab5a669fac4a408d
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-04-11 15:23:27 +0700

    refactor: link click and token buy modal color

--- components/Chapter/ChapterList.js
+++ components/Chapter/ChapterList.js
@@ -1,4 +1,5 @@
 import { formatNearAmount } from 'near-api-js/lib/utils/format'
+import Link from 'next/link'
 import * as React from 'react'
 import { parseImgUrl } from 'utils/common'
 import { parseDate } from 'utils/dateHelper'
@@ -64,27 +65,43 @@ const ChapterList = ({
   }
 
   return (
-    <div
-      className={`sm:h-40 h-36 flex mb-4 items-end ${className}`}
-      onClick={onClick}
-      {...rest}
-    >
+    <div className={`sm:h-40 h-36 flex mb-4 items-end ${className}`} {...rest}>
       <div className={listWrapperStyle}>
-        <div className={listCoverStyle} style={chapterImage} />
-        <div className="sm:ml-36 ml-24 pl-1 w-full flex flex-col sm:flex-row sm:items-center items-start justify-between">
-          <div className="mr-4">
-            <h5 className="sm:text-sm text-xs font-semibold text-gray-500 mb-1 uppercase">
-              Chapter {parseInt(data.metadata.chapter_id)}
-            </h5>
-            <h3 className="text-black font-semibold text-sm sm:text-lg">
-              {data.metadata.title}
-            </h3>
-            <h5 className="text-gray-800 sm:text-sm text-xs">
-              {parseDate(data.metadata.issued_at)}
-            </h5>
-          </div>
-          <h4 className={listBadgeStyle}>{textRight()}</h4>
-        </div>
+        <Link
+          href={`/comics/${data.metadata.comic_id}/chapter?chapterId=${data.metadata.chapter_id}`}
+        >
+          <a onClick={(e) => e.preventDefault()}>
+            <div
+              className={listCoverStyle}
+              style={chapterImage}
+              onClick={onClick}
+            />
+          </a>
+        </Link>
+        <Link
+          href={`/comics/${data.metadata.comic_id}/chapter?chapterId=${data.metadata.chapter_id}`}
+        >
+          <a
+            onClick={(e) => {
+              e.preventDefault()
+              onClick()
+            }}
+            className="sm:ml-36 ml-24 pl-1 w-full flex flex-col sm:flex-row sm:items-center items-start justify-between"
+          >
+            <div className="mr-4">
+              <h5 className="sm:text-sm text-xs font-semibold text-gray-500 mb-1 uppercase">
+                Chapter {parseInt(data.metadata.chapter_id)}
+              </h5>
+              <h3 className="text-black font-semibold text-sm sm:text-lg">
+                {data.metadata.title}
+              </h3>
+              <h5 className="text-gray-800 sm:text-sm text-xs">
+                {parseDate(data.metadata.issued_at)}
+              </h5>
+            </div>
+            <h4 className={listBadgeStyle}>{textRight()}</h4>
+          </a>
+        </Link>
       </div>
     </div>
   )
--- components/Comic/ComicItem.js
+++ components/Comic/ComicItem.js
@@ -1,35 +1,40 @@
 import { useRouter } from 'next/router'
 import { parseImgUrl } from 'utils/common'
 import Token from 'components/Token/Token'
+import Link from 'next/link'
 
 const ComicItem = ({ data, key, showLike = false }) => {
   const router = useRouter()
 
   return (
     <div key={key}>
-      <div
-        className="mb-2 cursor-pointer hover:opacity-90 transition ease-in-out duration-300"
-        onClick={() => router.push(`/comics/${data.comic_id}/chapter`)}
+      <a
+        onClick={(e) => e.preventDefault()}
+        href={`/comics/${data.comic_id}/chapter`}
       >
-        <Token
-          imgUrl={parseImgUrl(data.media, null, {
-            width: `300`,
-          })}
-          imgBlur="U0Csjj-;fQ-;%MfQfQfQfQfQfQfQ%MfQfQfQ"
-          hoverMouse={false}
-          initialRotate={{ x: 0, y: 0 }}
-          shadow="none"
-          borderRadius="0px"
-          disableFlip
-        />
-      </div>
-      <div className="ml-1">
-        <h4
-          className="text-black font-bold text-xl md:text-2xl truncate cursor-pointer hover:text-opacity-80"
+        <div
+          className="mb-2 cursor-pointer hover:opacity-90 transition ease-in-out duration-300"
           onClick={() => router.push(`/comics/${data.comic_id}/chapter`)}
         >
-          {data.title}
-        </h4>
+          <Token
+            imgUrl={parseImgUrl(data.media, null, {
+              width: `300`,
+            })}
+            imgBlur="U0Csjj-;fQ-;%MfQfQfQfQfQfQfQ%MfQfQfQ"
+            hoverMouse={false}
+            initialRotate={{ x: 0, y: 0 }}
+            shadow="none"
+            borderRadius="0px"
+            disableFlip
+          />
+        </div>
+      </a>
+      <div className="ml-1">
+        <Link href={`/comics/${data.comic_id}/chapter`}>
+          <a className="text-black font-bold text-xl md:text-2xl truncate cursor-pointer hover:text-opacity-80">
+            {data.title}
+          </a>
+        </Link>
         <p className="text-comic-gray-primary font-bold text-xs mb-3 truncate">
           {data.author_ids}
         </p>
@@ -40,12 +45,11 @@ const ComicItem = ({ data, key, showLike = false }) => {
             </p>
           </div>
         )}
-        <p
-          className="text-primary font-bold text-sm cursor-pointer hover:opacity-80 w-1 whitespace-nowrap"
-          onClick={() => router.push(`/comics/${data.comic_id}/chapter`)}
-        >
-          See Chapters
-        </p>
+        <Link href={`/comics/${data.comic_id}/chapter`}>
+          <a className="text-primary font-bold text-sm cursor-pointer hover:opacity-80 whitespace-nowrap">
+            See Chapters
+          </a>
+        </Link>
       </div>
     </div>
   )
